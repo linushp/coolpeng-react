b2a01643e63da2dbf85f5ab6d3b2b734f6a4a16e
luanhaipeng <910010653@qq.com>
2016-10-20 15:55:48 +0800

    aaa

--- src/components/carousel/jQueryCarousel.js
+++ src/components/carousel/jQueryCarousel.js
@@ -16,23 +16,23 @@ function getPlaceHolder() {
 }
 
 function encodeHTML(m) {
-    if(!m){
+    if (!m) {
         return "";
     }
-    m = m.replace(/</gm,"&lt;");
-    m = m.replace(/>/gm,"&gt;");
+    m = m.replace(/</gm, "&lt;");
+    m = m.replace(/>/gm, "&gt;");
     return m;
 }
 
 
-function getHeight(){
+function getHeight() {
     var scrollHeight = document.body.scrollHeight;//可以滚动区域高度
     var clientHeight = document.documentElement.clientHeight;//浏览器可见区域的高度
     var allHeight = Math.max(scrollHeight, clientHeight);
     return allHeight;
 }
 
-function getWidth(){
+function getWidth() {
     var scrollWidth = document.body.scrollWidth;//可以滚动区域高度
     var clientWidth = document.documentElement.clientWidth;//浏览器可见区域的高度
     var allWidth = Math.max(scrollWidth, clientWidth);
@@ -44,28 +44,36 @@ function createImageListHTML(imageList, targetImage) {
     var targetOid = targetImage.oid || "______$$$$_____";
     for (var i = 0; i < imageList.length; i++) {
         var img = imageList[i];
+        var ownerUser = img.ownerUser || {};
+
         var oid = img.oid;
-        var isCurrent = (oid === targetOid) || (img.oSrc===targetImage.oSrc);
+        var isCurrent = (oid === targetOid) || (img.oSrc === targetImage.oSrc);
         var src = isCurrent ? img.oSrc : '';
-        var imageItemClazz = isCurrent ? IMG_ITEM_CUR_CLASS:'';
-        var isFirst = (i===0);
-        var isLast = (i===(imageList.length-1));
-        if(isFirst){
-            imageItemClazz +=" isFirst ";
+        var imageItemClazz = isCurrent ? IMG_ITEM_CUR_CLASS : '';
+        var isFirst = (i === 0);
+        var isLast = (i === (imageList.length - 1));
+        if (isFirst) {
+            imageItemClazz += " isFirst ";
         }
-        if(isLast){
-            imageItemClazz +=" isLast ";
+        if (isLast) {
+            imageItemClazz += " isLast ";
         }
         var topHeight = 0;
-        var imageObjWrapperHeight = getHeight() -60;
+        var imageObjWrapperHeight = getHeight() - 60;
         html.push('' +
-            '<div class="'+IMG_ITEM_CLASS+'  '+imageItemClazz+'">' +
+            '<div class="' + IMG_ITEM_CLASS + '  ' + imageItemClazz + '">' +
             '   <div class="closeCarousel">×</div>' +
             `   <div class="preImage">←</div>` +
             `   <div class="nextImage">→</div>` +
-            '   <div class="imageName"> ' + encodeHTML(img.name) + ' </div>' +
-            '   <div class="imageObjWrapper" style="height: '+imageObjWrapperHeight+'px">' +
-            '       <img class="imageObj" src="' + src + '" osrc="' + img.oSrc + '" alt="' + img.name + '" >' +
+            '   <div class="imageName"> ' +
+            '       <div class="ownerUserAvatar">' +
+            '           <img  src="' + (ownerUser.avatar || "") + '" alt="">' +
+            '       </div>' +
+            '       <div class="ownerUserNickname">' + encodeHTML(ownerUser.nickname) + '</div>' +
+            '       <div class="imaName">' + encodeHTML(img.name) + '</div>' +
+            '   </div>' +
+            '   <div class="imageObjWrapper" style="height: ' + imageObjWrapperHeight + 'px">' +
+            '       <img class="imageObj" src="' + src + '" osrc="' + img.oSrc + '" alt="' + encodeHTML(img.name) + '" >' +
             '   </div>' +
             '</div>'
         );
@@ -75,12 +83,12 @@ function createImageListHTML(imageList, targetImage) {
 
 function showImageCarousel(imageList, targetImage) {
     var $placeholder = getPlaceHolder();
+    debugger;
     var imageListHtml = createImageListHTML(imageList, targetImage);
-    $placeholder.html('<div class="jQueryCarousel">' + imageListHtml +'</div>');
+    $placeholder.html('<div class="jQueryCarousel">' + imageListHtml + '</div>');
     $placeholder.show();
     initEvent();
 }
-
 
 
 var isInited = false;
@@ -91,25 +99,24 @@ function initEvent() {
     isInited = true;
 
 
-
     $(document).on('click', `#${PLACEHOLDER_ID} .nextImage`, function () {
         var $this = $(this);
         var $imgItem = $this.closest(`.${IMG_ITEM_CLASS}`);
-        if($imgItem.hasClass('isLast')){
+        if ($imgItem.hasClass('isLast')) {
             return;
         }
         $imgItem.removeClass(IMG_ITEM_CUR_CLASS);
         var $nextItem = $imgItem.next();
         var $nextImg = $nextItem.find(".imageObj");
         var osrc = $nextImg.attr('osrc');
-        $nextImg.attr('src',osrc);
+        $nextImg.attr('src', osrc);
         $nextItem.addClass(IMG_ITEM_CUR_CLASS);
     });
 
     $(document).on('click', `#${PLACEHOLDER_ID} .preImage`, function () {
         var $this = $(this);
         var $imgItem = $this.closest(`.${IMG_ITEM_CLASS}`);
-        if($imgItem.hasClass('isFirst')){
+        if ($imgItem.hasClass('isFirst')) {
             return;
         }
 
@@ -117,7 +124,7 @@ function initEvent() {
         var $nextItem = $imgItem.prev();
         var $nextImg = $nextItem.find(".imageObj");
         var osrc = $nextImg.attr('osrc');
-        $nextImg.attr('src',osrc);
+        $nextImg.attr('src', osrc);
         $nextItem.addClass(IMG_ITEM_CUR_CLASS);
     });
 
--- src/views/ChatRoom/MessageList/MessageCarousel.js
+++ src/views/ChatRoom/MessageList/MessageCarousel.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 import jQueryCarousel from '../../../components/carousel/jQueryCarousel';
 
-function getMessageImage($obj) {
+function getMessageImage($obj,msgObj) {
     var osrc = $obj.attr("osrc");
     var name = $obj.attr("alt") || "";
     var osrcwidth = $obj.attr('osrcwidth');
@@ -12,16 +12,32 @@ function getMessageImage($obj) {
         name: name,
         oSrc: osrc,
         oSrcHeight: osrcheight,
-        oSrcWidth: osrcwidth
+        oSrcWidth: osrcwidth,
+        ownerUser:msgObj && msgObj.sendUser
     }
 }
 
+function toMessageMap(messageList){
+    messageList = messageList.toJS();
+    var m = {};
+    messageList.forEach(function(v){
+        var msgId = v.msgId;
+        m[msgId] = v;
+    });
+    return m;
+}
 
 function getMessageImageList(divId,messageList) {
+
+    var msgMap = toMessageMap(messageList);
+
     var imageList = [];
     $('#' + divId).find(".chat-uploaded-image").each(function () {
         var $this = $(this);
-        var imgObj = getMessageImage($this);
+        var $msgItem = $this.closest('.chat-msg-item');
+        var msgId = $msgItem.attr('data-mid');
+        var msgObj = msgMap[msgId];
+        var imgObj = getMessageImage($this,msgObj);
         imageList.push(imgObj);
     });
     return imageList;
