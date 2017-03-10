ae0a0d9228f186ae270ee98e634c4f757497d733
master@ubibi.cn <master@ubibi.cn>
2017-03-10 16:46:01 +0800

    aaa

--- src/views/Page_Chat/MessageInput/MessageInput.js
+++ src/views/Page_Chat/MessageInput/MessageInput.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import RebixFlux from 'react-rebixflux';
 import RebixUtils from 'rebix-utils';
+const trim = RebixUtils.trim;
 const createPureComponent = RebixFlux.createPureComponent;
 const PureRenderComponent = RebixFlux.PureRenderComponent;
 const getDeepValue = RebixUtils.getDeepValue;
@@ -16,22 +17,51 @@ class MessageInput extends PureRenderComponent {
 
     handleSendMsg = ()=> {
         var {selSession} = this.props;
-        var MessageTextInput = this.refs['MessageTextInput'];
-        var value = MessageTextInput.value;
+        var MessageInputTextArea = this.refs['MessageInputTextArea'];
+        var value = trim(MessageInputTextArea.value || '');
+        if (value.length === 0) {
+            return;
+        }
+
+        MessageInputTextArea.value = '';
         MessageActions.sendMessage(selSession, {
             msg_type: 'text', //text,image,code
             msg_content: value
         });
+
         //发送消息后,滚动到最底部
         scrollMessageListToBottom();
     };
 
+    handleKeyDown=(e)=>{
+        if (e.keyCode !== 13) {
+            return;
+        }
+        if (e.keyCode === 13 && (e.shiftKey || e.ctrlKey)) {
+            return;
+        }
+
+        if (e.keyCode === 13) {
+            this.handleSendMsg();
+            e.preventDefault();
+            return false;
+        }
+    };
+
     render() {
         var that = this;
+        var textName = 'MessageInputTextArea';
         return (
             <div className="MessageInput">
-                <input type="text" ref="MessageTextInput"/>
-                <button onClick={that.handleSendMsg}>发送</button>
+                <div className="toolbar">
+                    <div className="toolbar_item send_emotion">表情</div>
+                    <div className="toolbar_item send_image">图片</div>
+                    <div className="toolbar_item send_code">代码</div>
+                    <div className="clear"></div>
+                </div>
+                <textarea ref={textName} className={textName} onKeyDown={that.handleKeyDown}></textarea>
+                <div className="sendButtonText">SHIFT + ENTER 换行，ENTER 直接发送</div>
+                <button className="sendButton" onClick={that.handleSendMsg}>发送</button>
             </div>
         )
     }
